10437ff537b1ebc8019b514309ce21c86ac1e1e6
Alexandre Chau <accounts@chau.moe>
2020-04-27 16:15:48 +0200

    Updates

---
+++ precache-manifest.8521da45ed7c0b4c6ba9ef81de21733e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e0048275307bf694d99a50aee01d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67750e1733d50ab3fc79",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260d135e8d8b717bf145",
+    "url": "/static/js/2.9598ca2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9598ca2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67750e1733d50ab3fc79",
+    "url": "/static/js/main.8d199dfa.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
