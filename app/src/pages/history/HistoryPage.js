9741b3fc9edd6f582ddb414bb67efe8c1dd827de
Alexandre Chau <accounts@chau.moe>
2020-05-01 02:29:00 +0200

    Implement color coding in histogram

--- app/src/pages/history/HistoryPage.js
+++ app/src/pages/history/HistoryPage.js
@@ -5,6 +5,7 @@ import { LoadingScreen } from '../../components/LoadingScreen'
 
 import './HistoryPage.sass'
 import scrollHintGif from './scroll_down.gif'
+import { ColorUtils } from '../../utils/ColorUtils'
 
 /**
  * History graph page component
@@ -49,33 +50,60 @@ class HistoryPage extends React.Component {
         this.wow.init()
     }
 
+    renderHistogram() {
+        /* Compute the color function: should rescale [min, max] # of episodes
+           into [light, dark] shades of given color */
+        const limits = {
+            maxCount: 50,
+            minCount: 0,
+            maxShade: -90,
+            minShade: 60
+        }
+        const ratio = (limits.maxShade - limits.minShade) / (limits.maxCount - limits.minCount)
+        const colorFunction = (color, episodeCount) => {
+            if (episodeCount > limits.maxCount) episodeCount = limits.maxCount
+            return ColorUtils.shade(color, ratio * episodeCount + limits.minShade)
+        }
+
+        return (
+            <div id="Histogram">
+                {Object.entries(this.state.data).map((yearToAnime, index) => {
+                    const [year, animes] = yearToAnime
+                    const animationDelay = `${index * 0.01 % 0.4}s`
+                    return (
+                        <div className="row wow fadeInUp" key={year} data-wow-delay={animationDelay}>
+                            <span className="year">{year}</span>
+                            <div className="dots">
+                                {animes.map(anime => {
+                                    const bgColor = { backgroundColor: colorFunction("#FFA500", anime.episodes) }
+                                    return (
+                                        <div className="dot" key={anime.anime_id} style={bgColor}>&nbsp;</div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+                <div id="HistogramBottom">
+                    <p>&nbsp;</p>
+                </div>
+                <div className="ScrollHint">
+                    <div onClick={this.scroll}>
+                        <img src={scrollHintGif} alt="Scroll down" />
+                        <p>Scroll down</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div id="History">
                 {this.state.loading ? <LoadingScreen /> : (
                     <div className="GraphView">
                         <div className="Left">
-                            <div id="Histogram">
-                                {Object.entries(this.state.data).map((yearToAnime, index) => {
-                                    const [year, animes] = yearToAnime
-                                    const animationDelay = `${index * 0.01 % 0.4}s`
-                                    return (
-                                        <div className="row wow fadeInUp" key={year} data-wow-delay={animationDelay}>
-                                            <span className="year">{year}</span>
-                                            <div className="dots">{animes.map(anime => <div className="dot" key={anime.anime_id}>&nbsp;</div>)}</div>
-                                        </div>
-                                    )
-                                })}
-                                <div id="HistogramBottom">
-                                    <p>&nbsp;</p>
-                                </div>
-                                <div className="ScrollHint" onClick={this.scroll}>
-                                    <div>
-                                        <img src={scrollHintGif} alt="Scroll down" />
-                                        <p>Scroll down</p>
-                                    </div>
-                                </div>
-                            </div>
+                            {this.renderHistogram()}
                         </div>
                         <div className="Right">
 
