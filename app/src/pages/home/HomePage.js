0b0bcb08556952eefd1fac607d1ead180aab5474
Alexandre Chau <accounts@chau.moe>
2020-05-28 02:49:49 +0200

    Add tips on home page

--- app/src/pages/home/HomePage.js
+++ app/src/pages/home/HomePage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFirefoxBrowser } from '@fortawesome/free-brands-svg-icons'
+import { faArchive, faDesktop, faRedoAlt } from '@fortawesome/free-solid-svg-icons'
 
 import './HomePage.sass'
 import { ConfigPanel } from '../../Config'
@@ -27,7 +30,15 @@ class HomePage extends React.Component {
                     </div>
                     <div className="Foreground">
                         <h1>Anime Data Visualization</h1>
-                        <p>An interactive data visualization experience by Alexandre CHAU, Pedro TORRES DA CUNHA & Joachim DUNANT for the COM-480 course</p>
+                        <p>An interactive data visualization experience based on the MyAnimeList dataset by Alexandre CHAU, Pedro TORRES DA CUNHA &amp; Joachim DUNANT for the COM-480 course</p>
+
+                        <p>
+                            For the best experience:
+                            <br /><FontAwesomeIcon icon={faFirefoxBrowser} /> Use Mozilla Firefox for perfomance
+                            <br /><FontAwesomeIcon icon={faArchive} /> Clear your browser cache (Ctrl + F5)
+                            <br /><FontAwesomeIcon icon={faDesktop} /> Use a 1920x1080 maximized window
+                            <br /><FontAwesomeIcon icon={faRedoAlt} /> Refresh this page for more backgrounds :)
+                        </p>
                         <ConfigPanel />
                         <button onClick={() => this.props.linkTo('/history')}>Let's start!</button>
                     </div>
